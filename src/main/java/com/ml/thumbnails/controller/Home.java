910f91042c29c65caeb11772c19dfbc3b8bbe17d
94203729 <94203729@qq.com>
2020-09-18 21:50:35 +0800

    修改下载

--- src/main/java/com/ml/thumbnails/controller/Home.java
+++ src/main/java/com/ml/thumbnails/controller/Home.java
@@ -3,8 +3,8 @@ package com.ml.thumbnails.controller;
 import com.ml.thumbnails.service.ExcelUtil;
 import lombok.Cleanup;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.OutputStream;
@@ -14,19 +14,38 @@ import java.io.OutputStream;
  * @author hjj
  * @createTime 2020/9/14
  */
-@RestController
+@Controller
 public class Home {
     @GetMapping("home")
     public String getPath(HttpServletResponse response)throws Exception{
-        @Cleanup OutputStream os=response.getOutputStream();
+      //  @Cleanup OutputStream os=response.getOutputStream();
         //给文件命名。随机命名
-        String fileName = "Excel-" + String.valueOf(System.currentTimeMillis()).substring(4, 13) + ".xlsx";
+       // String fileName = "Excel-" + String.valueOf(System.currentTimeMillis()).substring(4, 13) + ".xlsx";
         //告诉浏览器数据格式，将头和数据传到前台
-        String headStr = "attachment; filename=\"" + fileName + "\"";
-        response.setContentType("APPLICATION/OCTET-STREAM");
-        response.setHeader("Content-Disposition", headStr);
-        @Cleanup  Workbook workbook= ExcelUtil.createWorkbook(ExcelUtil.ExcelFileType.Excel2007);
-        workbook.write(os);
-        return "34";
+     //   String headStr = "attachment; filename=\"" + fileName + "\"";
+     //   response.setContentType("APPLICATION/OCTET-STREAM");
+     //   response.setHeader("Content-Disposition", headStr);
+      //  @Cleanup  Workbook workbook= ExcelUtil.createWorkbook(ExcelUtil.ExcelFileType.Excel2007);
+      //  workbook.write(os);
+        return "home";
+    }
+    @GetMapping("download")
+    public String getDownloadPath(HttpServletResponse response)throws Exception{
+          @Cleanup OutputStream os=response.getOutputStream();
+        //给文件命名。随机命名
+           String fileName = "Excel-" + String.valueOf(System.currentTimeMillis()).substring(4, 13) + ".xlsx";
+        //告诉浏览器数据格式，将头和数据传到前台
+           //String headStr = "attachment; filename=\"" + fileName + "\"";
+            // 解决导出文件名中文乱码
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/octet-stream;charset=UTF-8");
+            response.setHeader("Content-Disposition", String.format("attachment; filename=%s", new String(fileName.getBytes("UTF-8"), "ISO-8859-1")));
+            response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+            response.setHeader("Pragma", "no-cache");
+            response.setHeader("Expires", "0");
+            response.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+          @Cleanup Workbook workbook= ExcelUtil.createWorkbook(ExcelUtil.ExcelFileType.Excel2007);
+          workbook.write(os);
+        return "home";
     }
 }
